941f94dd319192e7fe11e1712724e5c310e3baa2
Brandon Heine <brandonmheine@gmail.com>
2022-02-24 09:39:37 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,124 @@
+let myLibrary = [];
+
+const heading = document.querySelector("h1");
+
+const button = document.querySelector("button");
+button.addEventListener("click", makeBook);
+
+const addBook = document.querySelector(".creator");
+
+const bookContainer = document.querySelector(".book-container");
+
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", addBookToLibrary);
+
+const titleEntry = document.querySelector("#title");
+const authorEntry = document.querySelector("#author");
+const pageEntry = document.querySelector("#pages");
+const readEntry = document.querySelector("#read");
+
+function makeBook() {
+  addBook.classList.toggle("hidden");
+  heading.style.opacity = "50%";
+  bookContainer.style.opacity = "50%";
+}
+
+function addCard(title, author, pages, read) {
+  const newDiv = document.createElement("div");
+  newDiv.style.background = "green";
+  newDiv.style.padding = "1rem 2rem";
+  newDiv.style.textAlign = "center";
+  newDiv.style.alignItems = "center";
+  newDiv.style.display = "flex";
+  newDiv.style.flexDirection = "column";
+  newDiv.style.justifyContent = "space-around";
+  newDiv.style.height = "200px";
+  newDiv.style.borderRadius = "2rem";
+
+  const titlePlace = document.createElement("div");
+  titlePlace.style.fontSize = "1.75rem";
+  titlePlace.textContent = `${title}`;
+
+  const authorPlace = document.createElement("div");
+  authorPlace.style.fontSize = "1rem";
+  authorPlace.textContent = `Author: ${author}`;
+
+  const pagesPlace = document.createElement("div");
+  pagesPlace.textContent = `${pages} pages`;
+
+  const readPlace = document.createElement("button");
+  readPlace.classList.add("small-button");
+  readPlace.textContent = read ? "Read" : "Not Read";
+
+  newDiv.appendChild(titlePlace);
+  newDiv.appendChild(authorPlace);
+  newDiv.appendChild(pagesPlace);
+  newDiv.appendChild(readPlace);
+
+  bookContainer.appendChild(newDiv);
+}
+
+// Book constructor
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary(e) {
+  // Prevent page reload
+  e.preventDefault();
+
+  // Check for valid input
+  if (!titleEntry.checkValidity()) {
+    alert("Please enter a book title.");
+    return;
+  }
+  if (!authorEntry.checkValidity()) {
+    alert("Please enter an author name.");
+    return;
+  }
+  if (!pageEntry.checkValidity()) {
+    alert("Please enter how many pages the book is.");
+    return;
+  }
+
+  // Create new book with info from form
+  const bookToAdd = new Book(
+    titleEntry.value,
+    authorEntry.value,
+    pageEntry.value,
+    readEntry.checked
+  );
+
+  // Add book to Library and create the card for it. This needs work.
+  myLibrary.push(bookToAdd);
+  addCard(bookToAdd.title, bookToAdd.author, bookToAdd.pages, bookToAdd.read);
+
+  //Clear inputs and hide form
+  titleEntry.value = "";
+  authorEntry.value = "";
+  pageEntry.value = "";
+  readEntry.value = "";
+  addBook.classList.toggle("hidden");
+  heading.style.opacity = "100%";
+  bookContainer.style.opacity = "100%";
+}
+
+function checkInput() {
+  if (!titleEntry.checkValidity()) {
+    alert("Wrong");
+    return;
+  }
+  if (!authorEntry.checkValidity()) {
+    alert("Wrong");
+    return;
+  }
+  if (!pageEntry.checkValidity()) {
+    alert("Wrong");
+    return;
+  }
+}
